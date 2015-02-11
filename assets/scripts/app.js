cd85ae4f227dea81585fac9f4ab6c7d64166ccce
Kim Rasmussen <kir@twins.net>
2015-02-11 14:11:55 +0100

    New navigation setup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -26,94 +26,19 @@ var UI = {
     },
     Mobile: {
         init: function () {
-            if(UI.Device == "small" || UI.Device == "medium" ) {
+            if(UI.Device == "small") {
                 UI.Mobile.on();
             }
         },
         on: function () {
-            // open and close primary navigation
-            $('.nav-trigger').on('click', function(e) {
-                e.preventDefault();
-                if( $('#site-wrapper').hasClass('nav-is-visible') ) {
-                    UI.Mobile.closeNav();
-                } else { 
-                    $(this).addClass('nav-is-visible');
-                    $('#site-wrapper').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-                        $('body').addClass('overflow-hidden');
-                    });
-                }
-            }); 
-            // open and close search
-            $('.search-trigger').on('click', function(e){
-                e.preventDefault();
-                UI.Mobile.closeNav();
-            });
+           
         },
         off: function () {
-            $('.nav-trigger').off();
         },
 
         closeNav: function () {
-            $('.nav-trigger').removeClass('nav-is-visible');
-            $('#site-wrapper').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-                $('body').removeClass('overflow-hidden');
-            });
+
         } 
-    },
-
-    SlickSlideshow: {
-        init: function () {
-            if($('#slideshow').length) {
-                $('#slideshow').slick({
-                  dots: true,
-                  infinite: true,
-                  speed: 1200,
-                  fade: true,
-                  slidesToShow: 1,
-                  adaptiveHeight: true,
-                  autoplay: true,
-                  arrows: false,
-                  prevArrow: '<button type="button" class="slick-prev"><svg class="icon"><use xlink:href="/svgsprite.svg#icon-arrow-left"></use></svg></button>', 
-                  nextArrow: '<button type="button" class="slick-next"><svg class="icon"><use xlink:href="/svgsprite.svg#icon-arrow-right"></use></svg></button>'
-                });
-            }
-        }
-    },
-    
-    CookieInfo: {
-        // self-executing
-        init: function () {
-            if ($.cookie('cookie_info') == undefined) {
-                function startCookieInfo(data) {
-                    var cookieInfo = "<div class='cookie-info'><div class='row'><div class='small-12 large-12 columns'>"
-                        + "<h4>" + data.CookieHeading + "</h4></div>"
-                        + "<div class='small-9 large-9 columns'><p>" + data.CookieText + "<br /><a href='" + data.CookieReadMoreLink + "'>" + data.CookieReadMoreText + "</a></p></div>"
-                        + "<div class='small-3 large-3 columns'><button class='tiny button primary accept right'>" + data.CookieButtonText + "</button></div></div></div>";
-                    $("body").prepend(cookieInfo);
-
-                    // Close on click
-                    $(".cookie-info .accept").on("click", function () {
-                        UI.CookieInfo.setCookie();
-                        $(".cookie-info").slideUp("fast");
-                    });
-                    //Set cookie if msg has been visible for 3 sec
-                    setTimeout(function () { UI.CookieInfo.setCookie(); }, 3000);
-                }
-                $.ajax({
-                    type: 'GET',
-                    url: '/assets/json/getCookieInfo.txt',
-                    dataType: 'json',
-                    success: startCookieInfo,
-                    error: function () { console.log("Fejl - Cookieinformation"); }
-                });
-            }
-        },
-        setCookie: function () {
-            $.cookie('cookie_info', 'accepted', { expires: 365, path: '/' });
-        },
-        clear: function () {
-            $.removeCookie('cookie_info', { path: '/' });
-        }
     }
 }
 
@@ -122,8 +47,25 @@ $(function () {
     UI.Foundation.init();
     UI.Viewport.init();
     UI.Mobile.init();
-    //UI.SlickSlideshow.init(); 
-    //UI.CookieInfo.init();
+
+
+    $('#js-nav-trigger').on('click', function(e) {
+        e.preventDefault();
+
+        $this = $(this);
+        $menu = $('#js-nav-menu');
+
+        if($this.hasClass('nav-is-visible')) {
+            $menu.removeClass('is-visible');
+            $this.removeClass('nav-is-visible');
+        } else {
+            $menu.addClass('is-visible');
+            $this.addClass('nav-is-visible');
+        }        
+    }); 
+
+
+
 
     // Throttled resize function
     $(window).on('resize', Foundation.utils.throttle(function(e){
